9635427d60ec73ae4a286627588e0d48317b8fd3
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-09-04 10:36:36 +0200

    Change

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -7,6 +7,7 @@ namespace EchoServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Server.Start();
         }
     }
 }
---
+++ EchoServer/Server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    class Server
+    {
+        public static void Start()
+        {
+            Console.WriteLine("starting echo biatch");
+
+            TcpListener serverSocket = new TcpListener(IPAddress.Loopback, 7777);
+            //TcpListener serverSocket = new TcpListener(7777);
+
+            // Start server
+            serverSocket.Start();
+
+            // accept tcp client
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("server activated");
+
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; //enables automatic flushing
+
+            string message = sr.ReadLine();
+
+            Console.WriteLine("received message: " + message);
+            if (message != null)
+                sw.WriteLine(message.ToUpper());
+
+            ns.Close();
+            Console.WriteLine("net stream closed");
+            connectionSocket.Close();
+            Console.WriteLine("connection socket closed");
+            serverSocket.Stop();
+            Console.WriteLine("server stopped");
+
+
+
+        }
+    }
+}
