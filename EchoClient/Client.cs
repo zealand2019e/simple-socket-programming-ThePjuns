a1f99a6959a53bc3791f67d3f8ac3365ef2cc0e7
Stefan de Haas Hansen <stef9239@edu.easj.dk>
2020-09-04 12:41:38 +0200

    Client class oprettet. Tom metode i

---
+++ EchoClient/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
